41eb29c95ad750bb3c8489f35b63618ed24d3061
Mrini <myanh@moneylover.me>
2017-12-12 19:53:50 +0700

    update test

--- test.py
+++ test.py
@@ -19,9 +19,9 @@ class TestStringMethods(unittest.TestCase):
         # check that s.split fails when the separator is not a string
         with self.assertRaises(TypeError):
             s.split(2)
-
-    def test_is_anagram(self):
-        self.assertFalse(is_anagram('acb', 'cdd'))
+#
+#    def test_is_anagram(self):
+#        self.assertFalse(is_anagram('acb', 'cdd'))
 
 if __name__ == '__main__':
     unittest.main()

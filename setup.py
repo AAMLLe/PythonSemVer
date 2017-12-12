da4e076dc8febabd5a7414e0276307f5b684509c
Mrini <myanh@moneylover.me>
2017-12-12 19:40:51 +0700

    add travis file

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
-#import re
-#from setuptools import find_packages, setup
-#import sys
-#
-#
-#def _read_long_description():
-#    try:
-#        with open('readme.rst') as fd:
-#            return fd.read()
-#    except Exception:
-#        return None
-#
-#
-#with open('pakage/__init__.py', 'r') as fd:
-#    version = re.search(
-#        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-#        fd.read(),
-#        re.MULTILINE
-#    ).group(1)
-#
+import re
+from setuptools import find_packages, setup
+import sys
+
+
+def _read_long_description():
+    try:
+        with open('readme.rst') as fd:
+            return fd.read()
+    except Exception:
+        return None
+
+
+with open('pakage/__init__.py', 'r') as fd:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+        fd.read(),
+        re.MULTILINE
+    ).group(1)
+
 ##with open('requirements/base.txt', 'r') as fd:
 ##    requirements = fd.read().strip().split('\n')
 #
@@ -28,28 +28,28 @@ try:
 except ImportError:
     pass
 
-#setup(
-#    name='python-semantic-release',
-#    version=version,
-#    url='https://github.com/AAMLLe/PythonSemVer',
-#    author='AAMLLe',
-#    author_email='myanh@moneylover.me',
-#    description='Demo SemVer for Python Project',
-#    long_description=_read_long_description(),
-#    packages=find_packages(exclude='tests'),
-#    license='MIT',
-##    install_requires=requirements,
-#    entry_points='''
-#        [console_scripts]
-#        semantic-release=semantic_release.cli:main
-#    ''',
-#    include_package_data=True,
-#    classifiers=[
-#        'Programming Language :: Python',
-#        'Programming Language :: Python :: 2',
-#        'Programming Language :: Python :: 2.7',
-#        'Programming Language :: Python :: 3',
-#        'Programming Language :: Python :: 3.4',
-#    ]
-#)
+setup(
+    name='PythonSemVer',
+    version=version,
+    url='https://github.com/AAMLLe/PythonSemVer',
+    author='AAMLLe',
+    author_email='myanh@moneylover.me',
+    description='Demo SemVer for Python Project',
+    long_description=_read_long_description(),
+    packages=find_packages(exclude='test'),
+    license='MIT',
+#    install_requires=requirements,
+    entry_points='''
+        [console_scripts]
+        semantic-release=semantic_release.cli:main
+    ''',
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6.3',
+    ]
+)
 
